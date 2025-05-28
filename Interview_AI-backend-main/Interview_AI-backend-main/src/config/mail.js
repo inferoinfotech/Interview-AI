379b6afb56796998d1a44ebdc3f516c3fd3aaf3e
monil <patelmonil7908@gmail.com>
2025-05-28 10:16:21 +0530

    first commit

---
+++ Interview_AI-backend-main/Interview_AI-backend-main/src/config/mail.js
@@ -0,0 +1,26 @@
+const nodemailer = require("nodemailer");
+
+const transPorter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "kotakh311@gmail.com",
+    pass: "casv enss rkeh viaw",
+  },
+});
+
+async function senData(to, subject,html) {
+  const mailFormat = {
+    from: "inferoinfotech@gmail.com",
+    to: to,
+    subject: subject,
+    html: html,
+  };
+  await transPorter.sendMail(mailFormat, (err, info) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("mail sent");
+    }
+  });
+}
+module.exports = senData;
